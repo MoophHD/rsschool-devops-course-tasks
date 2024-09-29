f222b12757ba4a85407a7d1d4cfcdd8737eb757d
Danila Sizykh <sizykhdanila@gmail.com>
2024-09-29 18:36:59 +0200

    github actions role > update trust relationships

--- github_oidc.tf
+++ github_oidc.tf
@@ -1,10 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  thumbprint_list = [
-    "A5E82B8D8E827B6395C24982B90E59B95E4B7D84",
-  ]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
-
--- iam_roles.tf
+++ iam_roles.tf
@@ -1,7 +1,7 @@
 
 resource "aws_iam_role" "github_actions_role" {
   name               = var.role_name
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.github_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_full_access" {
